5bb8a6de3ece6edf35ef47dd506bf8b3117f911b
Alexis <alexismoreauavila@gmail.com>
2024-02-28 15:37:22 +0100

    exo 1 done

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-// Iteration 1: Names and Input
+const { config } = require("yargs");
 
+// Iteration 1: Names and Input
+const hacker1 = 'Ivan';
+console.log(`The driver name is ${hacker1}`);
+const hacker2 = 'Alexis';
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
