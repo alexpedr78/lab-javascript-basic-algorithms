93ad57750a3f29938cfcad14d07b4479e9d3c5cf
Ivan Stoyanov <ivanstoyanov@outlook.com>
2024-02-28 15:47:05 +0100

    do Iteration 2

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-const { config } = require("yargs");
+//const { config } = require("yargs");
 
 // Iteration 1: Names and Input
-const hacker1 = 'Ivan';
+const hacker1 = "Ivan";
 console.log(`The driver name is ${hacker1}`);
-const hacker2 = 'Alexis';
+const hacker2 = "Alexis";
 console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
-
+if (hacker1.length > hacker2.length) {
+  console.log(
+    `The driver has the longest name, it has ${hacker1.length} characters.`
+  );
+} else if (hacker1.length < hacker2.length) {
+  console.log(
+    `It seems that the navigator has the longest name, it has ${hacker2.length} characters.`
+  );
+} else {
+  console.log(
+    `Wow, you both have equally long names, ${hacker1.length} characters!`
+  );
+}
 
 // Iteration 3: Loops
